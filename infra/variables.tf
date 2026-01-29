fb8090ea6da2a62cbbb8fc22245940730bbf8387
mahdikhashan <mahdikhashan1@gmail.com>
2026-01-29 21:46:12 +0100

    add variablers file

---
+++ infra/variables.tf
@@ -0,0 +1,18 @@
+variable "gpu_worker_name" {
+  type        = string
+  default     = "whisper-l4-worker"
+  description = "Name of the instance"
+}
+
+variable "gpu_worker_zone" {
+  type = string
+  default = "asia-southeast1-a"
+  description = "Gpu worker zone"
+}
+
+variable "gpu_worker_type" {
+  type = string
+  default = "g2-standard-4"
+  description = "Gpu worker type"
+}
+
